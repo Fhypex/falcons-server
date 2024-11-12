1be3847ada1de5c8b01c31c6ac8719ab8c5629bb
Mehmet Hayrullah Özkul <109302500+mehmethayrullahozkul@users.noreply.github.com>
2024-11-12 09:52:27 +0300

    Cors config setup (#27)
    Co-authored-by: mehmethayrullahozkul <hayru@Hayrullah-MacBook-Air.local>

---
+++ account/internal/src/main/java/gtu/cse/se/altefdirt/aymoose/account/internal/infra/config/WebConfig.java
@@ -0,0 +1,18 @@
+package gtu.cse.se.altefdirt.aymoose.account.internal.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // TODO: Remove if both frontend and backend servers are running on the same
+    // domain or machine
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allowed HTTP methods
+                .allowedHeaders("*"); // Allowed headers
+    }
+}
