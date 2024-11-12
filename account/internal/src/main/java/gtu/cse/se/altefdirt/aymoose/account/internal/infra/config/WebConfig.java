bbdfac19b4473d2838fbde6162e0291851521aa5
Mehmet Hayrullah Özkul <109302500+mehmethayrullahozkul@users.noreply.github.com>
2024-11-12 10:34:53 +0300

    Update WebConfig.java

--- account/internal/src/main/java/gtu/cse/se/altefdirt/aymoose/account/internal/infra/config/WebConfig.java
+++ account/internal/src/main/java/gtu/cse/se/altefdirt/aymoose/account/internal/infra/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all origins
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allowed HTTP methods
                 .allowedHeaders("*"); // Allowed headers
     }
